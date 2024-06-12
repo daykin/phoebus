d8fe2648bcf6cb484c1ac5a1fd4d0a404e49e164
Evan Daykin <daykin@frib.msu.edu>
2024-06-12 15:54:00 -0400

    connect listeners to backends

--- app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/DisplayRuntimeInstance.java
+++ app/display/runtime/src/main/java/org/csstudio/display/builder/runtime/app/DisplayRuntimeInstance.java
@@ -526,7 +526,7 @@ public class DisplayRuntimeInstance implements AppInstance
         navigation.dispose();
     }
 
-    public DisplayModel getActiveModel()
+    DisplayModel getActiveModel()
     {
         return active_model;
     }
